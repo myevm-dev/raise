057df0ae42ef61cb3dafddb5295c72931f854c49
Fun Fund <yo.myevm@gmail.com>
2024-12-12 23:52:52 +0000

    .

--- src/components/AccountBalancesCard.tsx
+++ src/components/AccountBalancesCard.tsx
@@ -40,6 +40,33 @@ const initialCollections: Collection[] = [
     nftBalance: '0',
     mnftBalance: '0',
   },
+  {
+    id: 4,
+    name: 'DEGEN',
+    logo: './degenlogo.svg',
+    nftAddress: '0x0e342F41e1B96532207F1Ad6D991969f4b58e5a1',
+    mnftAddress: '0xb736fd496c15c7285a0e61d0ae24b6020d0da387',
+    nftBalance: '0',
+    mnftBalance: '0',
+  },
+  {
+    id: 5,
+    name: 'GLITCH ON APE',
+    logo: './glitchlogo.svg',
+    nftAddress: '0x7cA094eB7E2e305135A0c49835e394b0daca8C56',
+    mnftAddress: '0x25fcaceB144227A341C2E621369346247EE7F902',
+    nftBalance: '0',
+    mnftBalance: '0',
+  },
+  {
+    id: 6,
+    name: 'A KID ON APE',
+    logo: './akidlogo.svg',
+    nftAddress: '0x2bEa2b6Bad866b5cA62117855D4b5D8A6C996Db2',
+    mnftAddress: '0x1eD327c0FAD66dB2258268D9841001853cD13Ff1',
+    nftBalance: '0',
+    mnftBalance: '0',
+  },
 ];
 
 const ERC20_ABI = [
